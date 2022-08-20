b254484fa5232b4b7c64f73eddae1feb855ff53e
Joer <ra.joer@gmail.com>
2022-08-20 09:56:22 -0300

    updated footer

--- src/features/landing-page/sections/footer/views/FooterView/index.tsx
+++ src/features/landing-page/sections/footer/views/FooterView/index.tsx
@@ -1,33 +1,53 @@
 import { Box, IconButton, Link, Typography } from '@mui/material';
-import { RiLinkedinFill } from 'react-icons/ri';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { BsMedium } from 'react-icons/bs';
 import { Container } from 'shared/components/Container';
 import * as styles from './styles';
+import { SyntheticEvent, useCallback } from 'react';
 
 export const FooterView = () => {
+  const handleClick = useCallback((e: SyntheticEvent) => {
+    const link = e.currentTarget.getAttribute('data-link') as string;
+    window.open(link, '_blank');
+  }, []);
+
   return (
     <Container sx={styles.container}>
       <Box sx={styles.grid}>
-        <Box>
-          <Typography variant="h1" mb={1}>
-            UXVolution
-          </Typography>
-          <Typography maxWidth={288}>
-            Empowering Healthcare Projects with User Centered Design
-          </Typography>
+        <Box sx={styles.logo}>
+          <Box
+            component="img"
+            src="/features/landing-page/assets/uxv-logo-footer.svg"
+          />
         </Box>
-        <span />
-        <Box sx={styles.info}>
-          <Typography maxWidth={288}>E-mail: info@uxvolution.com </Typography>
-          <Link href="http://google.com" target="_blank">
-            <IconButton
-              aria-label="linkedin"
-              color="primary"
-              size="medium"
-              sx={styles.button}
-            >
-              <RiLinkedinFill />
-            </IconButton>
-          </Link>
+        <Box sx={styles.box}>
+          <Typography variant="h6">Contact:</Typography>
+          <Typography variant="body1">
+            <Link href="mailto:info@uxvolution.com">info@uxvolution.com</Link>
+          </Typography>
+          <Typography variant="body1">+351 967 146 879</Typography>
+        </Box>
+        <Box sx={styles.social}>
+          <IconButton
+            onClick={handleClick}
+            data-link="https://www.linkedin.com/company/uxvolution/"
+            sx={styles.icon}
+          >
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton
+            onClick={handleClick}
+            data-link="https://medium.com/uxvolution "
+            sx={styles.icon}
+          >
+            <BsMedium />
+          </IconButton>
+        </Box>
+        <Box sx={styles.box}>
+          <Typography variant="h6">Location:</Typography>
+          <Typography variant="body1">
+            Madan Parque – Rua dos Inventores, 2825-182 Caparica
+          </Typography>
         </Box>
       </Box>
       <Box sx={styles.copyright}>
--- src/features/landing-page/sections/footer/views/FooterView/styles.ts
+++ src/features/landing-page/sections/footer/views/FooterView/styles.ts
@@ -4,28 +4,55 @@ export const container: StylesProps = {
   bgcolor: 'background.paper'
 };
 
+export const content: StylesProps = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
+  gap: 4
+};
+
+export const logo: StylesProps = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: { xs: 'center', md: 'flex-start' }
+};
+
+export const box: StylesProps = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  width: '100%',
+  gap: 0.5,
+  textAlign: { xs: 'center', md: 'left' }
+};
+
 export const grid: StylesProps = {
   color: 'white',
-  width: '100%',
   display: 'grid',
-  gridTemplateColumns: 'repeat(3, minmax(300px 1fr))',
+  width: '100%',
+  gridTemplateColumns: { xs: '1fr', md: '1fr 180px 180px 180px' },
   rowGap: 3,
-  alignItems: 'center',
-  pb: 8,
-  borderBottom: '1px solid rgba(255, 255, 255, 0.1)'
+  gap: 2,
+  alignItems: 'center'
 };
 
 export const button: StylesProps = {
   border: '1px solid'
 };
 
-export const info: StylesProps = {
+export const social: StylesProps = {
   display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'flex-start',
-  alignItems: 'flex-start',
+  justifyContent: 'center',
+  alignItems: 'center',
   width: '100%',
   gap: 2
+};
+
+export const icon: StylesProps = {
+  color: 'white',
+  border: '1px solid',
+  borderColor: 'grey.300'
 };
 
 export const copyright: StylesProps = {
